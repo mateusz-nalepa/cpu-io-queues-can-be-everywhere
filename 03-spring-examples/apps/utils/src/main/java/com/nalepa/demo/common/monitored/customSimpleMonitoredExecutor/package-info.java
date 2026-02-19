cb74c370bed5457a90253064eb4f91ed39ac0dd4
Mateusz Nalepa <mateuszcholyn@gmail.com>
2026-02-19 22:45:09 +0100

    add package-info instead of readme

---
+++ 03-spring-examples/apps/utils/src/main/java/com/nalepa/demo/common/monitored/customSimpleMonitoredExecutor/package-info.java
@@ -0,0 +1,5 @@
+package com.nalepa.demo.common.monitored.customSimpleMonitoredExecutor;
+
+// ExecutorServiceMetrics from `Micrometer` works basically like this package plus much more
+//
+// https://docs.micrometer.io/micrometer/reference/reference/jvm.html
--- 03-spring-examples/apps/utils/src/main/java/com/nalepa/demo/common/monitored/customSimpleMonitoredExecutor/readme.md
+++
@@ -1,5 +0,0 @@
-ExecutorServiceMetrics from `Micrometer` works basically like this package plus much more.
-
-https://docs.micrometer.io/micrometer/reference/reference/jvm.html
-
-https://github.com/micrometer-metrics/micrometer/pull/7083/changes
