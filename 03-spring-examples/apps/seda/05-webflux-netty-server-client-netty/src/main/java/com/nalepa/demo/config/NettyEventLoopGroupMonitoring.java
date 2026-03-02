376e063f2b5dd33d9cc7dfc33043a9ee957f1959
Mateusz Nalepa <mateuszcholyn@gmail.com>
2026-03-02 16:41:00 +0100

    fix monitoring for netty eventLoop server

--- 03-spring-examples/apps/seda/05-webflux-netty-server-client-netty/src/main/java/com/nalepa/demo/config/NettyEventLoopGroupMonitoring.java
+++ 03-spring-examples/apps/seda/05-webflux-netty-server-client-netty/src/main/java/com/nalepa/demo/config/NettyEventLoopGroupMonitoring.java
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Component;
 import reactor.netty.http.server.HttpServer;
 
 import java.time.Duration;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -34,8 +32,9 @@ public class NettyEventLoopGroupMonitoring {
 
         @Override
         public HttpServer apply(HttpServer server) {
-            return server.doOnChannelInit((observer, channel, remoteAddress) -> {
-                EventExecutorGroup group = channel.eventLoop().parent();
+            // doOnBound it's like doOnStart
+            return server.doOnBound((disposableServer) -> {
+                EventExecutorGroup group = disposableServer.channel().eventLoop().parent();
                 eventLoopLagMonitor.registerGroup(group);
             });
         }
@@ -44,40 +43,33 @@ public class NettyEventLoopGroupMonitoring {
     public static class EventLoopLagMonitor {
 
         private final MeterRegistry meterRegistry;
-        private final Duration interval = Duration.ofMillis(100);
-        private final Set<EventExecutor> registeredEventExecutors = ConcurrentHashMap.newKeySet();
+        private final Duration interval = Duration.ofMillis(20);
 
         public EventLoopLagMonitor(MeterRegistry meterRegistry) {
             this.meterRegistry = meterRegistry;
         }
 
         public void registerGroup(EventExecutorGroup group) {
-            group.forEach(this::registerExecutor);
-        }
-
-        private void registerExecutor(EventExecutor executor) {
-            if (!registeredEventExecutors.add(executor)) {
-                return;
-            }
-            monitor(executor);
+            group.forEach(this::monitor);
         }
 
         private void monitor(EventExecutor executor) {
             AtomicLong last = new AtomicLong(System.nanoTime());
 
-            executor.scheduleAtFixedRate(() ->
-                            executor.execute(() -> {
-                                long now = System.nanoTime();
-                                long diff = now - last.getAndSet(now);
+            executor.scheduleAtFixedRate(() -> {
+                        long now = System.nanoTime();
+                        long diff = now - last.getAndSet(now);
 
-                                long lagNanos = diff - interval.toNanos();
+                        long lagNanos = diff - interval.toNanos();
 
-                                if (lagNanos > 0) {
-                                    meterRegistry
-                                            .timer("custom.netty.eventloop.lag")
-                                            .record(lagNanos, TimeUnit.NANOSECONDS);
-                                }
-                            }),
+                        if (lagNanos > 0) {
+                            meterRegistry
+                                    .timer("custom.netty.eventloop.lag",
+                                            "executor", Thread.currentThread().getName()
+                                    )
+                                    .record(lagNanos, TimeUnit.NANOSECONDS);
+                        }
+                    },
                     0, interval.toMillis(), TimeUnit.MILLISECONDS
             );
         }
