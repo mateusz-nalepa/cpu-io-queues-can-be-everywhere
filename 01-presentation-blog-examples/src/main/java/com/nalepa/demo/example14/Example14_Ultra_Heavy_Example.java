d87ae418e5fd5b91871cb9cef3514edb638f951c
Mateusz Nalepa <mateuszcholyn@gmail.com>
2026-05-16 22:43:14 +0200

    fix easter egg

--- 01-presentation-blog-examples/src/main/java/com/nalepa/demo/example14/Example14_Ultra_Heavy_Example.java
+++ 01-presentation-blog-examples/src/main/java/com/nalepa/demo/example14/Example14_Ultra_Heavy_Example.java
@@ -36,9 +36,17 @@ public class Example14_Ultra_Heavy_Example {
     static class HelloWorld {
         static void println() {
             System.out.println("When the queue time grow, the system is slow");
-            System.out.println("When the queue is size, look for time elsewhere-0");
+            System.out.println("When the queue is zero, look for time elsewhere-0");
             System.out.println("Maybe async, maybe tweak, maybe system hit it's peak");
             System.out.println("Maybe faster CPU, to help system push straight through");
+            System.out.println();
+            System.out.println("Response times not flat?");
+            System.out.println("Something may be bad");
+            System.out.println("Something may be bad");
+            System.out.println("Maybe remove context-switch");
+            System.out.println("Maybe let's context-switch");
+            System.out.println("Both are two sides of same thing");
+            System.out.println("Performance monitoring!");
         }
     }
 }
