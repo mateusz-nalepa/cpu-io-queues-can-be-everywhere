3f2bd08b8fec486117275e9fb36d351ae53f8884
Mateusz Nalepa <mateuszcholyn@gmail.com>
2026-05-16 23:39:59 +0200

    add comments

--- javascript-examples/index.js
+++ javascript-examples/index.js
@@ -2,7 +2,8 @@ import {bigProcessing} from "./processingDefault/processingDefault.js";
 import {limitedBigProcessing} from "./processingSEDA/processingSEDA.js";
 
 console.log("############### Default");
-
+// this will block event loop for a long time
+// tasks are being executed synchronously
 await Promise.all([
     bigProcessing(1),
     bigProcessing(2)
@@ -15,6 +16,10 @@ console.log();
 console.log();
 console.log("############### SEDA");
 
+// this won't block event loop
+// tasks are being executed asynchronously
+// but thanks to mutex, from logical point of view,
+// they are still executed one after another,
 await Promise.all([
     limitedBigProcessing(1),
     limitedBigProcessing(2)
